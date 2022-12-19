e9e5db2d1d5d151fbd6a0c7833626a669e88a5e5
cyberkongzs <94583829+cyberkongzs@users.noreply.github.com>
2022-12-19 19:01:21 +0700

    update

---
+++ daftarBuku.php
@@ -0,0 +1,31 @@
+<?php
+include 'connect.php';
+
+// Memeriksa apakah request yang dikirim adalah GET
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    // Menjalankan query
+    $result = mysqli_query($connect, "SELECT * FROM data_buku");
+
+    // Membuat array untuk menampung hasil query
+    $response = array();
+
+    // Menampilkan hasil query
+    if (mysqli_num_rows($result) > 0) {
+        // Output data satu per satu
+        while ($row = mysqli_fetch_assoc($result)) {
+            array_push($response, array(
+                "kd_buku" => $row["kd_buku"],
+                "nama_buku" => $row["nama_buku"],
+                "jenis_buku" => $row["jenis_buku"],
+                "semester" => $row["semester"],
+                "tingkatan" => $row["tingkatan"],
+                "jumlah" => $row["jumlah"]
+            ));
+        }
+    } else {
+        $response['message'] = "Tidak ada data";
+    }
+
+    // Menampilkan respon dalam format JSON
+    echo json_encode($response);
+}
