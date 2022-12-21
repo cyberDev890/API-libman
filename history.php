bccc1f69b47884b0f06ca362819122720d95f89a
cyberkongzs <94583829+cyberkongzs@users.noreply.github.com>
2022-12-21 21:53:52 +0700

    menambah file baru

---
+++ history.php
@@ -0,0 +1,21 @@
+
+<?php
+include 'connect.php';
+$nis = $_GET['NIS'];
+
+$query = "SELECT riwayat.nama_buku, data_buku.semester, riwayat.tanggal_pengembalian FROM `riwayat` JOIN data_buku ON riwayat.kd_buku = data_buku.kd_buku WHERE riwayat.NIS = $nis";
+// Menjalankan query dan menyimpan hasilnya dalam variabel
+$result = mysqli_query($connect, $query);
+// Inisialisasi array kosong untuk menampung hasil
+$data = array();
+// Menyimpan hasil query ke dalam array
+while ($row = mysqli_fetch_assoc($result)) {
+    $data[] = $row;
+}
+// Mengeluarkan data dalam format JSON
+if (count($data) > 0) {
+    echo json_encode($data);
+} else {
+    echo json_encode('Data Kosong');
+}
+?>
---
+++ tindakan.php
@@ -0,0 +1,23 @@
+<?php
+include 'connect.php';
+$nis = $_GET['NIS'];
+
+$query = "SELECT data_buku.nama_buku,pengembalian.id_kembali,data_buku.semester, pengembalian.tanggal_pengembalian FROM `pengembalian` JOIN data_buku ON pengembalian.kd_buku=data_buku.kd_buku WHERE pengembalian.NIS='$nis'";
+// Menjalankan query dan menyimpan hasilnya dalam variabel
+$result = mysqli_query($connect, $query);
+// Inisialisasi array kosong untuk menampung hasil
+$data = array();
+// Menyimpan hasil query ke dalam array
+while ($row = mysqli_fetch_assoc($result)) {
+    $data[] = $row;
+}
+// Mengeluarkan data dalam format JSON
+if (count($data) > 0) {
+    echo json_encode($data);
+} else{
+    echo json_encode('Data Kosong');
+}
+?>
+
+
+
